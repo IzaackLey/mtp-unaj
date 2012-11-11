2e361e694dbd735107c11877e1333a5d72b52246
Pablo <pablogon@gmail.com>
2012-11-11 19:39:03 -0300

    agregado en decorator el tamano

---
+++ AbtractVsInterface/src/Poligono.java
@@ -0,0 +1,13 @@
+
+public abstract class Poligono {
+	int lados;
+	public void setLados(int l){
+		lados = l;
+	}
+	public int getLados(){
+		return lados;
+	}
+	
+	public abstract double getArea();
+	
+}
