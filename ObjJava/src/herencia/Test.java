02973be2deaa064a818689a8f8d0c8552451b7bc
pablo <pablogon@gmail.com>
2013-03-05 01:33:40 -0300

    avance de la filmina de introduccion a objetos

--- ObjJava/src/herencia/Test.java
+++ ObjJava/src/herencia/Test.java
@@ -1,10 +1,6 @@
 package herencia;
 import com.trolltech.qt.gui.*;
 public class Test extends QWidget{
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		FiguraGeometrica fig1 = new Circulo(10); 
 		System.out.println("el area del ciculo es " + fig1.getArea());
--- ObjJava/src/modificadores/Main.java
+++ ObjJava/src/modificadores/Main.java
@@ -8,5 +8,9 @@ public class Main {
 	    System.out.println("Hay " + Usuario.usuarios + " usuarios"); 
 	    //o desde una instacia cualquiera de la clase
 	    System.out.println("Hay " + raul.usuarios + " usuarios");  
+	    System.out.println("primer valor: " + Calculador.bascara(1, -7, 10)[0]);
+	    System.out.println("primer segundo: " + Calculador.bascara(1, -7, 10)[1]);
+	    
+	    
 	}
 }
