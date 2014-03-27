e8806a935df6e06f7547173a9e81f724ab24d97e
Pablo <pablogon@gmail.com>
2014-03-27 16:38:00 -0300

    entrada por teclado de la opcion

--- ObjJava/src/herencia/Test.java
+++ ObjJava/src/herencia/Test.java
@@ -1,29 +1,42 @@
 package herencia;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Test{
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("opcion 0, 1, 2:");
+        try{
+        	ClaseAbstracta.opcion = Integer.parseInt(br.readLine());
+        }catch(NumberFormatException nfe){
+            System.err.println("Invalid Format!");
+        }
+	        	    
 		FiguraGeometrica figuraGeometrica;
 		
 		if (ClaseAbstracta.opcion==0){
-			figuraGeometrica = new Circulo(10);//downcasting
+			figuraGeometrica = new Circulo(10);
 		}else if (ClaseAbstracta.opcion==1){
-			figuraGeometrica = new Cuadrado(10);//downcasting
+			figuraGeometrica = new Cuadrado(10);
 		}else{
-			figuraGeometrica = new Triangulo(6,6,6);//downcasting
+			figuraGeometrica = new Triangulo(6,6,6);
 		}
 		
 		Double perimetro = 0.0;
 		if (figuraGeometrica instanceof Triangulo) {
-			Triangulo triangulo  = (Triangulo) figuraGeometrica;//upcasting
+			Triangulo triangulo  = (Triangulo) figuraGeometrica; //downcasting
 			perimetro = Calculador.getPerimetro(triangulo);
 			triangulo.print();
 		}else if (figuraGeometrica instanceof Cuadrado){
-			Cuadrado cuadrado = (Cuadrado) figuraGeometrica;//upcasting
+			Cuadrado cuadrado = (Cuadrado) figuraGeometrica; //downcasting
 			perimetro = Calculador.getPerimetro(cuadrado);
 		}else if (figuraGeometrica instanceof Circulo){
-			Circulo circulo = (Circulo) figuraGeometrica;//upcasting
+			Circulo circulo = (Circulo) figuraGeometrica; //downcasting
 			perimetro = Calculador.getPerimetro(circulo);
 		}
 		System.out.println("el perimetro es " + perimetro.toString());
-		
-	}
+	};	
 }
