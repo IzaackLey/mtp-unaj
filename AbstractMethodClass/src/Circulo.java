e12ac0ef37075ff32f00e05e84b138b590a94f13
Pablo <pablogon@gmial.com>
2015-04-01 19:59:50 -0300

    nuevos proyectos para los videos

---
+++ AbstractMethodClass/src/Circulo.java
@@ -0,0 +1,20 @@
+
+
+public class Circulo extends FiguraGeometrica{
+	
+	public Circulo(double radio) {
+		super("Circulo");
+		this.radio = radio;
+	}
+
+
+	public double radio;
+
+
+	public double getArea() {
+		return Math.PI*Math.pow(radio,2);
+	}
+	public String toString() {
+		return String.format("figura:%s radio:%f, area:%f",nombre, radio, getArea());
+	}
+}
---
+++ AbstractMethodClass/src/Cuadrado.java
@@ -0,0 +1,19 @@
+
+
+public class Cuadrado extends FiguraGeometrica{
+	public double lado; 	
+		
+	public Cuadrado(double lado){
+		super("Cuadrado");
+		this.lado = lado;
+	}
+
+	public double getArea() {
+		return Math.pow(this.lado,2);
+	}
+
+	public String toString() {
+		return String.format("figura:%s lado:%f, area:%f",nombre, lado, getArea());
+	}
+
+}
